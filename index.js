ca3c3530461cb6cebb58956f592f3af4dc46984d
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-02-03 13:03:25 +0000

    Generate all html elements via for loop

--- index.js
+++ index.js
@@ -42,13 +42,32 @@ const message = document.getElementById("message");
 const likesCounter = document.getElementById("likes-counter");
 const userName = document.getElementById("user-name");
 const paraComment = document.getElementById("para-comment");
+const postsContainer = document.querySelector("body");
 
 for (let i=0; i < posts.length; i++) {
-    posterAvatar.src = posts[i].avatar;
-    posterName.textContent = posts[i].name;
-    posterLocation.textContent = posts[i].location;
-    post.src = posts[i].post;
-    likesCounter.textContent = `${posts[i].likes} likes`;
-    userName.textContent = posts[i].username;
-    paraComment.textContent = posts[i].comment;
+    const postElement = document.createElement("section");
+    postElement.innerHTML = `
+        <div class="poster">
+            <img src="${posts[i].avatar}" id="posters-avatar" alt="Poster's profile picture">
+            <div class="posters-information">
+                <h1 id="posters-name">${posts[i].name}</h1>
+                <p id="posters-location">${posts[i].location}</p>                
+            </div>
+        </div>
+        <img id="post" alt="Portrait of Poster" src="${posts[i].post}">
+        <div class="below-post">
+            <div class="icons">
+                <img id="like" alt="Heart Icon" src="images/icon-heart.png">
+                <img id="comment" alt="Speech Bubble" src="images/icon-comment.png">
+                <img id="message" alt="Paper Airplane" src="images/icon-dm.png">
+            </div>
+            <h2 id="likes-counter">${posts[i].likes} likes</h2>
+            <div class="likes-and-comments">
+                <h2 id="user-name">${posts[i].username}</h2>
+                <p id="para-comment">${posts[i].comment}</p>
+            </div>
+        </div>
+    `;
+
+    postsContainer.appendChild(postElement);
 }
